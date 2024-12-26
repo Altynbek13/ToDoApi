fda5a68bd5fdaf30bfe29f0cf640c971b9de0fff
Altynbek <aton.zhumabergen@mail.ru>
2024-12-26 15:59:38 +0500

    Добавьте файлы проекта.

---
+++ ToDoApi/TodoDb.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoApi
+{
+    public class TodoDb:DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options):base(options)
+        {
+            
+        }
+        public DbSet<TodoItem> Todos { get; set; }
+    }
+}
